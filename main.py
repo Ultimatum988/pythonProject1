62def79415b4878c4b16e2241657d5090809078e
admin <prud.dmitriy@yandex.ru>
2023-10-21 16:44:06 +0700

    Lesson 1

---
+++ main.py
@@ -0,0 +1,36 @@
+from telegram.ext import Updater, CommandHandler
+import requests
+import re
+
+
+def get_url():
+    contents = requests.get('https://aws.random.cat/meow.json').json()
+    url = contents['url']
+    return url
+
+
+def get_image_url():
+    allowed_extension = ['jpg', 'jpeg', 'png']
+    file_extension = ''
+    while file_extension not in allowed_extension:
+        url = get_url()
+        file_extension = re.search("([^.]*)$", url).group(1).lower()
+    return url
+
+
+def bop(update, context):
+    url = get_image_url()
+    chat_id = update.message.chat_id
+    context.bot.send_photo(chat_id=chat_id, photo=url)
+
+
+def main():
+    updater = Updater('6328487396:AAHdA7zu3_ShToSnig4xrUyVNuW5p45H4-E', use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('bop', bop))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
